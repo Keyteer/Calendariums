53a1f80ac3de0bceb54350fb24ba13ba10f352f7
avargas2020 <avargas2020@inf.udec.cl>
2025-11-15 20:15:06 -0300

    Configuración de Docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,19 @@
+
 import 'dotenv/config';
 import express from "express";
 
 // Rutas
 import eventsRoutes from "./routes/events.routes.js";
 
-
+require('dotenv').config();
 const app = express()
 app.use(express.json());
+const port = process.env.PORT || 6969;
 
 // Ruta de prueba
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 // Registrar rutas
 app.use("/events", eventsRoutes);
