1b3b411aa49ad655392e5c0aab4719d463140b61
Aneira22 <alneira2017@inf.udec.cl>
2025-11-17 23:47:46 -0300

    Docker funcional

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,8 @@
-
-import 'dotenv/config';
 import express from "express";
-
 // Rutas
 import eventsRoutes from "./routes/events.routes.js";
-
-require('dotenv').config();
 const app = express()
 app.use(express.json());
-const port = process.env.PORT || 6969;
-
 // Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Hello World!');
